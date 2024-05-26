d21e434b0498a4f847393608eba04609d38508ab
omiras <oscar.miras@gmail.com>
2024-05-26 13:20:53 +0200

    fix console.log

--- algoritmia/2-match-uefa-euro.js
+++ algoritmia/2-match-uefa-euro.js
@@ -10,7 +10,7 @@ function uefaEuro2016(teams, scores) {
 
 }
 
-uefaEuro2016(['Germany', 'Ukraine'], [2, 0]) // "At match Germany - Ukraine, Germany won!"
-uefaEuro2016(['Belgium', 'Italy'], [0, 2]) // "At match Belgium - Italy, Italy won!"
-uefaEuro2016(['Portugal', 'Iceland'], [1, 1]) // "At match Portugal - Iceland, teams played draw."
-uefaEuro2016(['Italy', 'Spain'], [0, 2]) // "At match Italy - Spain, Italy won!"
+console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0])) // "At match Germany - Ukraine, Germany won!"
+console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2])) // "At match Belgium - Italy, Italy won!"
+console.log(uefaEuro2016(['Portugal', 'Iceland'], [1, 1])) // "At match Portugal - Iceland, teams played draw."
+console.log(uefaEuro2016(['Italy', 'Spain'], [2, 0])) // "At match Italy - Spain, Italy won!"
